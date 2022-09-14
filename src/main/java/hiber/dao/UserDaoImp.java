f3e07d52dd768607321b34f3bd95e303fd57d1f3
stas-nevskijj <102740749+stas-nevskijj@users.noreply.github.com>
2022-09-14 14:35:58 +0300

    Fix imports

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -1,7 +1,6 @@
 package hiber.dao;
 
 import hiber.model.User;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -2,7 +2,14 @@ package hiber.model;
 
 import org.hibernate.annotations.Cascade;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "test.users")
