0477fa963bf13095dd2fb68b15d575680efa45fc
stas-nevskijj <102740749+stas-nevskijj@users.noreply.github.com>
2022-09-14 14:31:15 +0300

    Done

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,43 @@
+package hiber.dao;
+
+import hiber.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+
+   @Override
+   public User getUserByCar(String model, int series) {
+      Query query = sessionFactory.getCurrentSession().createQuery(
+              "select user from Car car where car.model = :model and car.series = :series"
+      );
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+      User user = (User) query.getSingleResult();
+      return user;
+
+   }
+
+}
